3725e5607e580522dfd776f1bb37473f46f3cbfb
dhruv thakur <dhruvthakur8006080@gmail.com>
2025-12-07 23:54:23 +0530

    25

---
+++ 3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("Name : Dhruv Kumar\n");
+    printf("College : Shoolini University\n");
+    return 0;
+}
---
+++ 9.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    printf("Int = %lu\n",sizeof(int));
+    printf("Float = %lu\n",sizeof(float));
+    printf("Double = %lu\n",sizeof(double));
+    printf("Char = %lu\n",sizeof(char));
+    return 0;
+}
