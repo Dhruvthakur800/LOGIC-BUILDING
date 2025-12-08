b52699e0d35e55acb50af8bd7b870b722024b102
dhruv thakur <dhruvthakur8006080@gmail.com>
2025-12-08 07:02:14 +0530

    other c prog

---
+++ 30.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int n,temp,rem,sum=0;
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    temp=n;
+    while(n!=0){
+        rem=n%10;
+        sum+=rem*rem*rem;
+        n/=10;
+    }
+
+    if(temp==sum)
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong");
+
+    return 0;
+}
